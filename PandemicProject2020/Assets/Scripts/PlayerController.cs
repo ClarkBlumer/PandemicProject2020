f43e637335cf6d4f865fa166ed8bcf2a788cd57e
ClarkBlumer <jarrad.blumer@gmail.com>
2020-09-16 17:29:37 -0700

    Basic movements added
    Moves left, right, jump.  basic.

---
+++ PandemicProject2020/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+//using UnityEngine.InputSystem;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 0;
+    public bool isGrounded = false;
+
+    private Rigidbody2D rb;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Jump();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * Speed;
+        
+        
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        }
+    }
+}
